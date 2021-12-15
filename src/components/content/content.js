fc8e219eebda6b8964a47afe9ccbbdb59a297ab5
Vincent9235 <vincentf.laurens@orange.fr>
2021-12-15 13:20:42 +0100

    add button & collect word

--- src/components/content/content.js
+++ src/components/content/content.js
@@ -1,15 +1,26 @@
 import React, { useContext } from 'react';
 import './content.css';
 import { ThemeContext } from '../../Context/themeContext';
+import Button from '../button/button';
 
 export default function Content() {
-  
+
     const { theme } = useContext(ThemeContext);
+
+    //Call API
+    
+    //Word
+    const fetchWord = async () => {
+        fetch('https://animalfinderapi.herokuapp.com/word')
+            .then(Response => Response.json())
+            .then(json => console.log(json.data.word));
+    }
 
     return (
         <div className={theme ? 'content light' : 'content dark'}>
             <h1>Trouve l'animal</h1>
             <p>{theme}</p>
+            <Button value={'Génerer un mot'} onClick={() => fetchWord()} />
         </div>
     )
 }
