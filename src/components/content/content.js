dfcb0432a0761835f074cbc26279f109e52694c8
Vincent9235 <vincentf.laurens@orange.fr>
2021-12-15 16:34:25 +0100

    add leaderboard

--- src/components/content/content.js
+++ src/components/content/content.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import './content.css';
 import { ThemeContext } from '../../Context/themeContext';
 import Button from '../button/button';
@@ -7,8 +7,8 @@ export default function Content() {
 
     const { theme } = useContext(ThemeContext);
 
-    //Call API
-    
+    /*******************Call API***************/
+
     //Word
     const fetchWord = async () => {
         fetch('https://animalfinderapi.herokuapp.com/word')
@@ -16,10 +16,31 @@ export default function Content() {
             .then(json => console.log(json.data.word));
     }
 
+    const [score, setscore] = useState(undefined)
+    useEffect(() => {
+        (async () => {
+            const newScore = await getScore()
+            setscore(newScore)
+        })()
+        return () => { }
+    }, [])
+
+    //Score
+    const getScore = async () => {
+        const scoreJson = await fetch('https://animalfinderapi.herokuapp.com/score')
+        return await scoreJson.json()
+    }
+
+    if (!score) {
+        return <p>Waiting</p>
+    }
+
+    //Display content
     return (
         <div className={theme ? 'content light' : 'content dark'}>
             <h1>Trouve l'animal</h1>
             <p>{theme}</p>
+            <table><tbody>{score.data.map(result => <tr><td>{result.username}</td><td><img className="card-img" src={result.avatar}/></td><td>Score: {result.score}</td></tr>)}</tbody></table>
             <Button value={'Génerer un mot'} onClick={() => fetchWord()} />
         </div>
     )
